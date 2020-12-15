72b0766ae8f24b900be2cf7fd55f4db2356dcffe
Tyler Gerwig <eit_tcgerwig@iastate.edu>
2020-12-15 17:02:51 -0600

    Adding control panel for regional directors

---
+++ staff_profile/staff_profile_reed/src/Controller/RegionalDirectorPanel.php
@@ -0,0 +1,10 @@
+<?php
+namespace Drupal\staff_profile_reed\Controller;
+
+use \Drupal\Core\Controller\ControllerBase;
+/**
+ * Regional Director Panels
+ */
+class RegionalDirectorPanel extends ControllerBase {
+
+}
