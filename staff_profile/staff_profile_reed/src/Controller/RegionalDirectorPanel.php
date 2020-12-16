f13740667d02783cf2bff30722dd6b9affddcf57
Tyler Gerwig <eit_tcgerwig@iastate.edu>
2020-12-16 17:01:52 -0600

    Added controller that renders series of views for each county regional manager controls, working on only showing link to role regional_director

--- staff_profile/staff_profile_reed/src/Controller/RegionalDirectorPanel.php
+++ staff_profile/staff_profile_reed/src/Controller/RegionalDirectorPanel.php
@@ -2,9 +2,38 @@
 namespace Drupal\staff_profile_reed\Controller;
 
 use \Drupal\Core\Controller\ControllerBase;
+use \Drupal\user\Entity\User;
 /**
  * Regional Director Panels
  */
 class RegionalDirectorPanel extends ControllerBase {
+  /**
+   * Get tids of counties that regional director has listed as web editor for
+   */
+  private function getCountiesServed($netid) {
+    $nodes = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['field_staff_profile_netid' => $netid]);
+    if ($node = reset($nodes)) {
+      return $node->field_staff_profile_cty_author->referencedEntities();
+    } else {
+      return [];
+    }
+  }
+
+
+  public function panel() {
+    $user = User::load(\Drupal::currentUser()->id());
+    $counties = RegionalDirectorPanel::getCountiesServed($user->getUsername());
+    $result = [];
+    foreach ($counties as $key => $county) {
+      $result[] = [
+        '#type' => 'view',
+        '#name' => 'regional_director_county',
+        '#display_id' => 'block_1',
+        '#arguments' => [$county->tid],
+        '#embed' => TRUE,
+      ];
+    }
+    return $result;
+  }
 
 }
