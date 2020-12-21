ee2b0440a8a0d0402b8f5822a408953ed120eac7
Tyler Gerwig <eit_tcgerwig@iastate.edu>
2020-12-21 17:06:34 -0600

    Finished remove from county operation, adding permission status

--- staff_profile/staff_profile_reed/src/Form/StaffProfileRemoveCtyAuthor.php
+++ staff_profile/staff_profile_reed/src/Form/StaffProfileRemoveCtyAuthor.php
@@ -5,15 +5,44 @@ namespace Drupal\staff_profile_reed\Form;
 use Drupal\Core\Entity\ContentEntityConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * @ingroup staff_profile_reed
  */
 class StaffProfileRemoveCtyAuthor extends ContentEntityConfirmFormBase {
-  public function getQuestion() {
-    return $this->t('Are you sure you want to remove %name from county?', array('%name' => $this->entity->label()));
+  //County to be removed
+  private  $county;
+
+  //TODO: Look for a way to add this as a controller for staff_profile to automatically pick up entity
+  function __construct() {
+    $this->setEntityTypeManager(\Drupal::entityTypeManager());
+    $this->setEntityManager(\Drupal::entityManager());
+    $this->setModuleHandler(\Drupal::moduleHandler());
+    $path = explode('/', \Drupal::service('path.current')->getPath());
+    $nid = $path[2];
+    $tid = $path[4];
+    $entity = $this->entityTypeManager->getStorage('node')->load($nid);
+    $this->setEntity($entity);
+    $this->county = $this->entityTypeManager->getStorage('taxonomy_term')->load($tid);
   }
 
+  //NOTE: May not be needed when entity manager is removed in 9.x+
+  protected function initFormLangcodes(FormStateInterface $form_state) {
+    if (!$form_state->has('entity_default_langcode')) {
+      $form_state->set('entity_default_langcode', $this->entity->getUntranslated()->language()->getId());
+    }
+    if (!$form_state->has('langcode')) {
+      $form_state->set('langcode', \Drupal::languageManager()->getCurrentLanguage()->getId());
+    }
+  }
+
+  public function getQuestion() {
+    return $this->t('Are you sure you want to remove %name from %cty county?', array('%name' => $this->entity->label(), '%cty' => $this->county->label()));
+  }
+
+  //Fix Incoming
+  //https://www.drupal.org/project/drupal/issues/2582295
   public function getCancelUrl() {
     return new Url('staff_profile_reed.regional_director_panel');
   }
@@ -24,7 +53,12 @@ class StaffProfileRemoveCtyAuthor extends ContentEntityConfirmFormBase {
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $staff_profile = $this->getEntity();
-    $this->logger('staff_profile_reed')->notice('Removed %title from county editor', array('%title' => $this->entity->label()));
+    $ctys = $staff_profile->get('field_staff_profile_cty_author')->getValue();
+    $key = array_search($this->county->id(), array_column($ctys, 'target_id'));
+    $staff_profile->get('field_staff_profile_cty_author')->removeItem($key);
+    $staff_profile->save();
+
+    $this->logger('staff_profile_reed')->notice('Removed %title from county editor in %cty county', array('%title' => $this->entity->label(), '%cty' => $this->county->label()));
     $form_state->setRedirect('staff_profile_reed.regional_director_panel');
   }
 }
