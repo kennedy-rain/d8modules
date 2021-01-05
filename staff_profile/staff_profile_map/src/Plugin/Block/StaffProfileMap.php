a97fd11d6dd197b3cce0b89e071e529c6677c6cb
Brian Webster <bwebster@iastate.edu>
2021-01-05 14:07:50 -0600

    Added block configuration to the block

--- staff_profile/staff_profile_map/src/Plugin/Block/StaffProfileMap.php
+++ staff_profile/staff_profile_map/src/Plugin/Block/StaffProfileMap.php
@@ -4,6 +4,7 @@ namespace Drupal\staff_profile_map\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Field\FieldFilteredMarkup;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'Staff Profile Map' Block.
@@ -24,7 +25,8 @@ class StaffProfileMap extends BlockBase
   {
     //Initialize some variables
     $results = '';
-    $styles = $this->default_styles();
+    $config = $this->getConfiguration();
+    $styles = $this->default_styles($config['max_size']);
     $mapPath = \Drupal::service('file_system')->realpath(\Drupal::service('module_handler')->getModule('staff_profile_map')->getPath()) . '/iowa_map.svg';
     $mapCode = file_get_contents($mapPath);
     $displayMap = false;
@@ -36,7 +38,7 @@ class StaffProfileMap extends BlockBase
       foreach ($node->get('field_staff_profile_cty_served') as $ctyServed) {
         $county_name = $ctyServed->entity->label();
         if (!empty($county_name)) {
-          $styles .= '#' . $this->fixCounty($county_name) . ' polygon {fill:pink;}' . PHP_EOL;
+          $styles .= '#' . $this->fixCounty($county_name) . ' polygon {fill:' . $config['served_color'] . '}' . PHP_EOL;
           $displayMap = true;
         }
       }
@@ -45,7 +47,7 @@ class StaffProfileMap extends BlockBase
       if ($node->get('field_staff_profile_base_county')->entity != null) {
         $base_county = $node->field_staff_profile_base_county->entity->label();
         if (!empty($base_county)) {
-          $styles .= '#' . $this->fixCounty($base_county) . ' polygon {fill:red;}' . PHP_EOL;
+          $styles .= '#' . $this->fixCounty($base_county) . ' polygon {fill:' . $config['base_color'] . ';}' . PHP_EOL;
           $displayMap = true;
         }
       }
@@ -74,20 +76,80 @@ class StaffProfileMap extends BlockBase
     return 0;
   }
 
-  private function default_styles()
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state)
+  {
+    $config = $this->getConfiguration();
+
+    $form['max_size'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Maximum Width of Map, in pixels'),
+      //'#description' => t('Zero (0) means display all events'),
+      '#size' => 15,
+      '#default_value' => $config['max_size'],
+    );
+
+    $form['base_color'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Color to shade the Base County'),
+      //'#description' => t('Zero (0) means display all events'),
+      '#size' => 15,
+      '#default_value' => $config['base_color'],
+    );
+
+    $form['served_color'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Color to shade the Counties Served'),
+      //'#description' => t('Zero (0) means display all events'),
+      '#size' => 15,
+      '#default_value' => $config['served_color'],
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state)
+  {
+    $values = $form_state->getValues();
+
+    $this->configuration['max_size'] = $values['max_size'];
+    $this->configuration['base_color'] = $values['base_color'];
+    $this->configuration['served_color'] = $values['served_color'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration()
+  {
+    return array(
+      'max_size' => '400',
+      'base_color' => '#CC0000',
+      'served_color' => '#F1BE48',
+
+    );
+  }
+
+  private function default_styles($max_size)
   {
     $return_string = '';
 
 
     $return_string .= '/* Added for Staff Directory Fill */' . PHP_EOL;
 
-    $return_string .= 'svg {max-width:400px;}' . PHP_EOL;
+    $return_string .= 'svg {max-width:' . $max_size . 'px;}' . PHP_EOL;
     $return_string .= 'g.RegionNumber {display:none;}' . PHP_EOL;
     $return_string .= 'g.CountyName {display:none;}' . PHP_EOL;
     $return_string .= '#Region_1 polygon, #Region_2 polygon, #Region_3 polygon, #Region_4 polygon, #Region_5 polygon, #Region_6 polygon, #Region_7 polygon, #Region_8 polygon, #Region_9 polygon, #Region_10 polygon, #Region_11 polygon, #Region_12 polygon, #Region_13 polygon, #Region_14 polygon, #Region_15 polygon, #Region_16 polygon, #Region_17 polygon, #Region_18 polygon, #Region_19 polygon, #Region_20 polygon, #Region_21 polygon, #Region_22 polygon, #Region_23 polygon, #Region_24 polygon, #Region_25 polygon, #Region_26 polygon, #Region_27 polygon {fill:#ffffff; stroke:black;}' . PHP_EOL;
 
     return $return_string;
   }
+
   private function fixCounty($county_name)
   {
     $county_name = str_replace(' ', '_', $county_name);
