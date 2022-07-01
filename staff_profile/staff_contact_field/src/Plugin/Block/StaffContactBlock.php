b67f32b702cfa0bbe91a03e7396bb58484f81302
Brian Webster <bwebster@iastate.edu>
2022-07-01 16:27:25 -0500

    Handle multiple instances of field

--- staff_profile/staff_contact_field/src/Plugin/Block/StaffContactBlock.php
+++ staff_profile/staff_contact_field/src/Plugin/Block/StaffContactBlock.php
@@ -32,23 +32,36 @@ class StaffContactBlock extends BlockBase
     $nids = \Drupal::entityQuery('node')->condition('type', 'staff_profile')->condition('status', 1)->sort('field_staff_profile_last_name')->sort('field_staff_profile_first_name')->execute();
     $staff_nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
 
+    // Get the corrent node
     $node = \Drupal::routeMatch()->getParameter('node');
+
+    // Make sure it is a node
     if ($node instanceof \Drupal\node\NodeInterface) {
+      // Step through the fields of the node
       $fields = $node->getFields();
       foreach ($fields as $field) {
+        // Looking for staff_contact_field type(s)
         if ($field->getFieldDefinition()->getType() == 'staff_contact_field') {
-          $contacts = unserialize($field->getValue()[0]['contacts']);
-          arsort($contacts);
-          $results = '<ul class="staff_contact_list">' . PHP_EOL;
+          // Handle multiple values for one field
+          foreach ($field->getValue() as $conact_list) {
+            // Set up some html elements
+            $results .= '<div class="contact_group">' . PHP_EOL;
+            $contacts = unserialize($conact_list['contacts']);
+            arsort($contacts);
+            $results .= '<ul class="staff_contact_list">' . PHP_EOL;
 
-          foreach ($contacts as $key => $value) {
-            foreach ($staff_nodes as $staff_member) {
-              if ($staff_member->id() == $key) {
-                $results .= '<li><a href="' . $base_url . \Drupal::service('path_alias.manager')->getAliasByPath('/node/'. $staff_member->id()) . '">' . $staff_member->getTitle() . '</a></li>' . PHP_EOL;
+            foreach ($contacts as $key => $value) {
+              // Find the right staff member and output...
+              foreach ($staff_nodes as $staff_member) {
+                if ($staff_member->id() == $key) {
+                  $results .= '<li><a href="' . $base_url . \Drupal::service('path_alias.manager')->getAliasByPath('/node/' . $staff_member->id()) . '">' . $staff_member->getTitle() . '</a></li>' . PHP_EOL;
+                }
               }
             }
+             // close out html elements
+            $results .= '</ul>' . PHP_EOL;
+            $results .= '</div>' . PHP_EOL;
           }
-          $results .= '</ul>' . PHP_EOL;
         }
       }
     }
