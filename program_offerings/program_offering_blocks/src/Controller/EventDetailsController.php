1e597215b40984b72cec88ff9bd040d7d9ee07af
Brian Webster <bwebster@iastate.edu>
2023-08-16 16:26:05 -0500

    Changed the description on event details page

--- program_offerings/program_offering_blocks/src/Controller/EventDetailsController.php
+++ program_offerings/program_offering_blocks/src/Controller/EventDetailsController.php
@@ -52,18 +52,18 @@ class EventDetailsController extends ControllerBase
         }
         $results .= '  <div class="event_address">' . $event_address . '  </div>' . PHP_EOL;
 
-        if (!empty($event['Planned_Program__r.Web_Description__c'])) {
-          $description = str_replace('<p><br></p>', '', $event['Planned_Program__r.Web_Description__c']) . PHP_EOL;
-        } else {
-          $results .= $event['Program_Description__c'] . PHP_EOL;
-        }
+        $description = '';
+//        if (!empty($event['Planned_Program__r.Web_Description__c'])) {
+//          $description = str_replace('<p><br></p>', '', $event['Planned_Program__r.Web_Description__c']) . PHP_EOL;
+//        } else {
+          $description .= $event['Program_Description__c'] . PHP_EOL;
+//        }
         if (!empty($event['Planned_Program__r.Smugmug_ID__c'])) {
-          $description = '<img class="educational_program_image" src="https://photos.smugmug.com/photos/' . $event['Planned_Program__r.Smugmug_ID__c'] . '/0/XL/' . $event['Planned_Program__r.Smugmug_ID__c'] . '-XL.jpg" alt="" />' . $description . '<div class="clearer"></div>';
+          $description .= '<img class="educational_program_image" src="https://photos.smugmug.com/photos/' . $event['Planned_Program__r.Smugmug_ID__c'] . '/0/XL/' . $event['Planned_Program__r.Smugmug_ID__c'] . '-XL.jpg" alt="" />' . $description . '<div class="clearer"></div>';
         }
-        if (isset($description)) {
-          $results .= '  <div class="event_description">' . $description . PHP_EOL;
-        }
-
+//        if (isset($description)) {
+          $results .= '  <div class="event_description">' . $description . '</div>' . PHP_EOL;
+//        }
 
         $results .= '  <div class="event_contact_label">Contact Info:</div>' . PHP_EOL;
         $results .= '  <div class="event_contact_name">' . $event['Contact_Information_Name__c'] . '</div>' . PHP_EOL;
