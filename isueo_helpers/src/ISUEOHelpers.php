764a7f9813ca33be0402ad040d49c3b3d9f3ea26
Brian Webster <bwebster@iastate.edu>
2023-09-21 15:36:48 -0500

    Created the isueo_helpers module

---
+++ isueo_helpers/src/ISUEOHelpers.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\isueo_helpers;
+//use Drupal\Core\Controller\ControllerBase;
+
+class ISUEOHelpers {
+
+  public static function map_get_svg($styles = '')  {
+    $mapPath = \Drupal::service('file_system')->realpath(\Drupal::service('module_handler')->getModule('isueo_helpers')->getPath()) . '/images/iowa_map.svg';
+    $mapCode = file_get_contents($mapPath);
+
+    return str_replace('/*ReplaceMe*/', $styles, $mapCode);
+  }
+
+}
