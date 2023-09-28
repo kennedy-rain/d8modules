16a37a2a732879218bec0a69b7064dc1d587f181
Brian Webster <bwebster@iastate.edu>
2023-09-28 10:05:56 -0500

    Getting ready to try caching results

--- isueo_helpers/src/ISUEOHelpers/Files.php
+++ isueo_helpers/src/ISUEOHelpers/Files.php
@@ -8,6 +8,9 @@ use Drupal\Core\Site\Settings;
 class Files
 {
 
+  // 900 seconds = 15 minutes
+  public static const CACHE_SECONDS = 900;
+
   // Get a file from a URL
   // TODO: Add some sort of caching mechanism
   public static function fetch_url(string $url, bool $ok_to_cache = false)
@@ -26,10 +29,11 @@ class Files
         $creds .= '@';
       }
 
-      // Insert the credentials and get file
+      // Insert the credentials into the URL
       $url = str_replace('https://', 'https://' . $creds, $url);
     }
 
+    // Get the page and return the results
     return file_get_contents($url);
   }
 }
