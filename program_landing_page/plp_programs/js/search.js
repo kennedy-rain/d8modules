6b51078a30a8408fe8d529ac65a2551321849b0f
Brian Webster <bwebster@iastate.edu>
2024-04-16 11:46:30 -0500

    Added a search block

---
+++ program_landing_page/plp_programs/js/search.js
@@ -0,0 +1,60 @@
+    const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+        server: {
+            apiKey: 'bilLvsiWoO1EqcM21L8XrzofmVBYfyB9', // Be sure to use an API key that only allows searches, in production
+            nodes: [
+                {
+                    host: 'typesense.exnet.iastate.edu',
+                    port: '8108',
+                    protocol: 'https',
+                },
+            ],
+        },
+        // The following parameters are directly passed to Typesense's search API endpoint.
+        //  So you can pass any parameters supported by the search endpoint below.
+        //  queryBy is required.
+        //  filterBy is managed and overridden by InstantSearch.js. To set it, you want to use one of the filter widgets like refinementList or use the `configure` widget.
+        additionalSearchParameters: {
+            queryBy: 'title,body,field_plp_program_search_terms',
+        },
+    });
+    const searchClient = typesenseInstantsearchAdapter.searchClient;
+
+    const search = instantsearch({
+        searchClient,
+        indexName: 'plp_programs',
+    });
+
+
+    search.addWidgets([
+        instantsearch.widgets.searchBox({
+            container: '#searchbox',
+        }),
+        instantsearch.widgets.configure({
+            hitsPerPage: 12,
+        }),
+        instantsearch.widgets.hits({
+            container: '#hits',
+            templates: {
+                item(item) {
+                    programImage = '';
+                    if (item.field_plp_program_smugmug) {
+                      programImage = '<img src ="https://photos.smugmug.com/photos/' + item.field_plp_program_smugmug + '/0/XL/' + item.field_plp_program_smugmug + '-XL.jpg" height="100" alt="" />';
+                    }
+                    return `
+                        <div>
+                          ${programImage}
+                          <div class="hit-name">
+                            <a href="${item.url}">${item._highlightResult.title.value}</a>
+                          </div>
+                          <!-- <div class="${item.field_plp_program_num_events}_events">Number of events: ${item.field_plp_program_num_events}</div> -->
+                        </div>
+                      `;
+                },
+            },
+        }),
+        instantsearch.widgets.pagination({
+            container: '#pagination',
+        }),
+    ]);
+
+    search.start();
---
+++ program_landing_page/plp_programs/src/Plugin/Block/PLPProgramsSearch.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\plp_programs\Plugin\Block;
+use Drupal\Core\Field\FieldFilteredMarkup;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\isueo_helpers\ISUEOHelpers;
+
+/**
+ * Provides a 'PLP Programs Search' Block.
+ *
+ * @Block(
+ *   id = "plp_programs_search",
+ *   admin_label = @Translation("PLP Programs Search"),
+ *   category = @Translation("PLP"),
+ * )
+ */
+class PLPProgramsSearch extends BlockBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+
+    // Do NOT cache a page with this block on it
+    //\Drupal::service('page_cache_kill_switch')->trigger();
+
+    $results = '
+    <p>I am here</p>
+    <div class="container">
+        <div class="search-panel">
+            <div class="search-panel__results">
+                <div id="searchbox"></div>
+                <div id="hits"></div>
+            </div>
+        </div>
+
+        <div id="pagination"></div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.44.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typesense-instantsearch-adapter@2/dist/typesense-instantsearch-adapter.min.js"></script>
+    ';
+
+    //Add allowed tags for svg map
+    $tags = FieldFilteredMarkup::allowedTags();
+    array_push($tags, 'script', 'div', 'img', 'src');
+
+    $block = [];
+    $block['#allowed_tags'] = $tags;
+    $block['#markup'] = $results;
+    $block['#attached']['library'][] = 'plp_programs/plp_programs_search';
+    return $block;
+  }
+
+  /**
+   * @return int
+   */
+  public function getCacheMaxAge()
+  {
+    return 0;
+  }
+}
