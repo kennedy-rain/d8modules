44de7f259bb47606c1a2f2c177bce52d51351db5
Brian Webster <bwebster@iastate.edu>
2024-04-26 14:57:31 -0500

    Added a search field to the results area

--- cse_selector/src/Controller/ResultsController.php
+++ cse_selector/src/Controller/ResultsController.php
@@ -14,11 +14,12 @@ class ResultsController extends ControllerBase {
    */
   public function resultsPage() {
     \Drupal::service('page_cache_kill_switch')->trigger();
-    $form = \Drupal::formBuilder()->getForm('Drupal\cse_selector\Form\ResultsForm');
-    $form['form_id']['#access'] = FALSE;
-    $form['form_build_id']['#access'] = FALSE;
-    $form['form_token']['#access'] = FALSE;
-    $form['search']['search_submit']['#name'] = FALSE;
+    $form['Search'] = \Drupal::formBuilder()->getForm('Drupal\cse_selector\Form\CSESearchForm');
+    $form['Results'] = \Drupal::formBuilder()->getForm('Drupal\cse_selector\Form\ResultsForm');
+    $form['Results']['form_id']['#access'] = FALSE;
+    $form['Results']['form_build_id']['#access'] = FALSE;
+    $form['Results']['form_token']['#access'] = FALSE;
+    $form['Results']['search']['search_submit']['#name'] = FALSE;
     return $form;
   }
 }
