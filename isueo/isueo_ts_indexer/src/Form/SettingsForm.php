971174ee44b6716e4871f407e76cbbb75e336019
Brian Webster <bwebster@iastate.edu>
2024-05-01 14:00:06 -0500

    Added a module to index content from the site to Typesense

---
+++ isueo/isueo_ts_indexer/src/Form/SettingsForm.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Drupal\isueo_ts_indexer\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\isueo_helpers\ISUEOHelpers\Typesense;
+
+/**
+ * Class SettingsForm.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'isueo_ts_indexer.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('isueo_ts_indexer.settings');
+
+    $form['typesense']['api_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#description' => $this->t('API Key generated from the Typesense server'),
+      '#size' => 50,
+      '#maxlength' => 100,
+      '#default_value' => $config->get('api_key'),
+      '#required' => true,
+    ];
+
+    $form['collection'] = [
+      '#type' => 'textfield',
+      '#title' => t('Collection Name'),
+      '#description' => t('Typesense Collection to write to'),
+      '#size' => 50,
+      '#maxlength' => 100,
+      '#default_value' => empty($config->get('collection')) ? 'extension_content' : $config->get('collection'),
+      '#required' => true,
+    ];
+
+    $urlParts = explode('/' , $_SERVER['REQUEST_URI']);
+    $form['site_name'] = [
+      '#type' => 'textfield',
+      '#title' => t('Site Name'),
+      '#description' => t('Name of this Drupal site, should be the same as used in drush configuration'),
+      '#size' => 50,
+      '#maxlength' => 100,
+      '#default_value' => empty($config->get('site_name')) ? $urlParts[1] : $config->get('site_name'),
+      '#required' => true,
+    ];
+
+    $types = \Drupal::entityTypeManager()
+      ->getStorage('node_type')
+      ->loadMultiple();
+    $contentTypes = [];
+    foreach ($types as $type) {
+      $contentTypes[$type->id()] = $type->id();
+    }
+
+    $form['content_types'] = [
+      '#type' => 'checkboxes',
+      '#title' => t('Content Types'),
+      '#description' => t('Content Types you would like to include in the index'),
+      '#options' => $contentTypes,
+      '#default_value' => empty($config->get('content_types')) ? [] : array_values($config->get('content_types')),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    parent::submitForm($form, $form_state);
+
+    $contentTypes = [];
+    foreach ($form_state->getValue('content_types') as $type) {
+      if (!empty($type)) {
+        $contentTypes[$type] = $type;
+      }
+    }
+
+    $this->config('isueo_ts_indexer.settings')
+      ->set('api_key', $form_state->getValue('api_key'))
+      ->set('collection', $form_state->getValue('collection'))
+      ->set('site_name', $form_state->getValue('site_name'))
+      ->set('content_types', $contentTypes)
+      ->save();
+
+      isueo_ts_indexer_index_all_nodes();
+
+  }
+}
+
