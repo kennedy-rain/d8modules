17491d42dc67a92ee969eb57644c2d24e930eedc
Brian Webster <bwebster@iastate.edu>
2024-08-07 17:18:27 -0500

    EW-701: Changed the educational_programs_field module to use a TWIG Template

--- educational_programs/educational_programs_field/src/Plugin/Field/FieldFormatter/EducationalProgramsFieldDefaultFormatter.php
+++ educational_programs/educational_programs_field/src/Plugin/Field/FieldFormatter/EducationalProgramsFieldDefaultFormatter.php
@@ -56,15 +56,14 @@ class EducationalProgramsFieldDefaultFormatter extends FormatterBase {
       // Load the Taxonomy Term
       $term = Term::load($item->term_id);
       if ($term == null) {
-        $output = PHP_EOL . '<div class="educational_program_description">Program not found</div>' . PHP_EOL;
-        $elements[$delta] = array('#markup' => $output);
         continue;
       }
 
       // Find the description of the program by steppign through the list of Programs until we find the right one based on the MyData ID
-      $description = 'Program not found';
-      $website = "";
-      $website_description = "";
+      $website = '';
+      $description = '';
+      $redirected = false;
+
       foreach ($programs as $program) {
         if ($program['Id'] == trim(strip_tags($term->getDescription()))) {
           if (!empty($program['Web_Description__c'])) {
@@ -76,13 +75,8 @@ class EducationalProgramsFieldDefaultFormatter extends FormatterBase {
           } else {
             $description = 'Description not found';
           }
-          if (!empty($program['Smugmug_ID__c'])) {
-            $description = '<img class="educational_program_image" src="https://photos.smugmug.com/photos/' . $program['Smugmug_ID__c'] . '/0/XL/' . $program['Smugmug_ID__c'] . '-XL.jpg" alt="" />'. $description . '<div class="clearer"></div>';
-          }
-
           if (!empty($program['Planned_Program_Website__c'])) {
             $website = $program['Planned_Program_Website__c'];
-            $website_description = "More about " . $program['Name'];
           }
           break;
         }
@@ -95,35 +89,27 @@ class EducationalProgramsFieldDefaultFormatter extends FormatterBase {
         continue;
       }
 
-      // Render output
-      $output = PHP_EOL;
-      $output .= '<div class="educational_program">' . PHP_EOL;
-
       // Add a note if page is being redirected
       if (!empty($website) && !empty($item->auto_redirect)) {
-        $output .= '<div class="educational_program_redirected">' . PHP_EOL;
-        $output .= '<h4>Note: Page Redirected</h4>' . PHP_EOL;
-        $output .= '<p>Public users will automatically be redirected to <a href="' . $website . '">' . $website . '</a></p>' . PHP_EOL;
-        $output .= '</div>' . PHP_EOL;
+        $redirected = true;
       }
-
-      $output .= '<div class="educational_program_description">' . PHP_EOL;
-      $output .= $description . PHP_EOL;
-      $output .= '</div>' . PHP_EOL;
-      if (!empty($website)) {
-        $output .= '<div class="educational_program_link"><a href="' . $website . '">' . $website_description . '</a></div>' . PHP_EOL;
-      }
-      $output .= '</div>' . PHP_EOL;
 
       //May need this stuff
-      $tags = FieldFilteredMarkup::allowedTags();
-      array_push($tags, 'iframe', 'div', 'h2', 'h3', 'h4', 'h5', 'h5', 'h6', 'footer', 'article', 'img');
-      while (preg_match('/<iframe[a-zA-Z0-9\" =\/\._\?\%]+\/>/', $output, $matches, PREG_OFFSET_CAPTURE)) {
-        $output = substr_replace($output, "> </iframe>", strlen($matches[0][0])+$matches[0][1]-2, 11);
-      }
+      //$tags = FieldFilteredMarkup::allowedTags();
+      //array_push($tags, 'iframe', 'div', 'h2', 'h3', 'h4', 'h5', 'h5', 'h6', 'footer', 'article', 'img');
+      //while (preg_match('/<iframe[a-zA-Z0-9\" =\/\._\?\%]+\/>/', $description, $matches, PREG_OFFSET_CAPTURE)) {
+        //$description = substr_replace($description, "> </iframe>", strlen($matches[0][0])+$matches[0][1]-2, 11);
+      //}
 
-      //$elements[$delta] = array('#markup' => $output);
-      $elements[$delta] = array('#markup' => $output, '#allowed_tags' => $tags);
+      $elements[$delta] = [
+        //'#allowed_tags' => $tags,
+        '#theme' => 'educational_programs_field_default',
+        '#program_name' => $program['Name'],
+        '#description' => $description,
+        '#url' => $website,
+        '#smugmug_id' => $program['Smugmug_ID__c'],
+        '#redirected' => $redirected,
+      ];
     }
     return $elements;
   }
